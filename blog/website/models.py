9bfe8fc98baed71e71fb552b79f0f4f77c0d7a60
juliano <juliano85.jg@gmail.com>
2021-01-28 23:07:21 -0300

    Manipulação com arquivos de media

--- blog/website/models.py
+++ blog/website/models.py
@@ -16,6 +16,7 @@ class Post(models.Model):
     )
 
     deleted = models.BooleanField(default=True)
+    imagem = models.ImageField(upload_to='posts', null=True, blank=True)
 
     def __str__(self):
         return self.title
